ad734ac5ebaa50adde822868bf33b7e436fcfb18
Ian Frost <inf5000@gmail.com>
2016-07-04 00:37:54 -0400

    Got rid of jquery dependent nav functions and replaced it with vanilla JS. Need to reformat class names for sure.

--- dist/js/init.js
+++ dist/js/init.js
@@ -8,19 +8,5 @@
 		
 		//updateAds();
 	});
-
-
-	//Navigation toggle
-	$('.nav-toggle-menu').click(function(e) {
-		e.preventDefault();
-		$(this).toggleClass('active');
-		$('.nav').toggleClass('active');
-	});
-	
-	//Navigation toggle
-	$('.nav-toggle-search').click(function(e) {
-		e.preventDefault();
-		$(this).toggleClass('active');
-		$('.header .search-form').toggleClass('active');
-	});
 })(this);
+
