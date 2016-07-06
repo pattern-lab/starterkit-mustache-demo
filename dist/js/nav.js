2ebd348114701dd34c7bd29cda4d20c7a0e50706
Ian Frost <inf5000@gmail.com>
2016-07-06 17:30:51 -0400

    Designed blog index page and header

--- dist/js/nav.js
+++ dist/js/nav.js
@@ -1,33 +1,35 @@
 //Navigation toggle
 var navActive = false,
     navOpen = document.querySelector('.nav-toggle-menu'),
-    navListOpen = document.querySelector('.nav');
+    navListOpen = document.querySelector('.c-primary-nav');
 
 navOpen.addEventListener("click", function(event){
 	event.preventDefault();
 
     if (navActive === false) {
 	    navActive = true;
-	    navListOpen.classList.add("active");
-	} else {
+	    navListOpen.classList.add("c-primary-nav--is-active");
+	} 
+	else {
 		 navActive = false;
-		 navListOpen.classList.remove("active");
+		 navListOpen.classList.remove("c-primary-nav--is-active");
 	}
 });
 
 //Search form toggle
 var searchActive = false,
     searchOpen = document.querySelector('.nav-toggle-search'),
-    searchFormOpen = document.querySelector('.header .search-form');
+    searchFormOpen = document.querySelector('.c-search-form');
     
     searchOpen.addEventListener("click", function(event){
 	event.preventDefault();
 
     if (searchActive === false) {
 	    searchActive = true;
-	    searchFormOpen.classList.add("active");
-	} else {
+	    searchFormOpen.classList.add("c-search-form--is-active");
+	} 
+	else {
 		 searchActive = false;
-		 searchFormOpen.classList.remove("active");
+		 searchFormOpen.classList.remove("c-search-form--is-active");
 	}
 });
